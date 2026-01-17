164364682f707b423816b3765af67fe1f6b5a15e
sudovaibhav <vaibhav.b.g.patil2@gmail.com>
2026-01-17 09:38:53 +0530

    add

---
+++ emp.py
@@ -0,0 +1,2 @@
+def get_employee_details(name, emp_id, department, salary):
+    return f"Employee Name: {name}, Employee ID: {emp_id}, Department: {department}, Salary: {salary}"
---
+++ test_emp.py
@@ -0,0 +1,14 @@
+import pytest
+from p_03.emp import get_employee_details
+def test_get_employee_details():
+    name = "Alice Smith"
+    emp_id = "E202"
+    department = "HR"
+    salary = 60000
+    expected_output = (
+        "Employee Name: Alice Smith, "
+        "Employee ID: E202, "
+        "Department: HR, "
+        "Salary: 60000"
+    )
+    assert get_employee_details(name, emp_id, department, salary) == expected_output
